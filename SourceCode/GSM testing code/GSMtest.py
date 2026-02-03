91c393fe83d9eed3aa45f0afdd8be52ec44c6db2
Notech5 <noah.greenland@students.plymouth.ac.uk>
2026-02-03 13:00:21 +0000

    Updated GSMtest.py
    Updated GSMtest to work with the new GSM module which is so far functional and responds to AT and ATI status queries

--- SourceCode/GSM testing code/GSMtest.py
+++ SourceCode/GSM testing code/GSMtest.py
@@ -4,23 +4,35 @@ import machine
 from machine import UART
 import time
 
+#connect to GSM module
 gsm = machine.UART(0, baudrate=9600, tx=machine.Pin(0), rx=machine.Pin(1)) 
 
-
+#loop control
 run = True
 
 #counter variable
 i = 0
 
-
-loop for testing
+#loop for testing
 while run == True:
     
-       #increment counter variable
+    #increment counter variable
     i = i+1
     
+    #request firmware version and device status
+    gsm.write(b'ATI\r\n')
+    
+    #store output in data variable
     data = gsm.read()
     
+    #only runs if there is a return
+    if data:
+        
+        #decode data and then strip headers
+        text = data.decode('utf-8', 'ignore')
+        data = text.strip()
+    
+    #for testing
     #helps to separate data
     print("-----------------------------")
     print(i+1, "\n" , data)
