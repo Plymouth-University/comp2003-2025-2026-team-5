518c406edfc175f62669bcac6d82ff794b6705e7
Praise Agidee <agideedaniel@gmail.com>
2026-03-21 16:20:12 +0100

    Added stuff

--- Database Development/table_for_geofence.sql
+++ Database Development/table_for_geofence.sql
@@ -1,9 +1,9 @@
 CREATE TABLE geofence_v1.geofence (
-    geofence_id UNIQUEIDENTIFIER PRIMARY KEY,
+    geofence_id UNIQUEIDENTIFIER PRIMARY KEY DEFAULT NEWID(),
     patient_id UNIQUEIDENTIFIER NOT NULL,
     name VARCHAR(100),
     shape_type VARCHAR(20),
-    encrypted_payload VARBINARY NOT NULL,
+    encrypted_payload VARBINARY(MAX) NOT NULL,
     is_active BIT DEFAULT 1,
     created_at TIMESTAMP NOT NULL,
     created_by UNIQUEIDENTIFIER NOT NULL,
---
+++ Database Development/test_data_1.sql
@@ -0,0 +1,2 @@
+INSERT INTO geofence_v1.account (email, role, is_active, created_at)
+VALUES ('john.doe@example.com', 'admin', 1, DEFAULT);
